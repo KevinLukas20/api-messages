d2a2e61f591a3ef0bf517b9eeac7290ea8307b58
Kevin Lucas <134294523+KevinLukas20@users.noreply.github.com>
2024-07-15 11:34:47 -0300

    Update router.js
    dfaswdgf

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const router = express.Router();
-
 const messagesController = require('./controllers/messagesControllers');
-const messagesMiddleware = require('./middlewares/messagesMiddleware')
+const messagesMiddleware = require('./middlewares/messagesMiddleware');
+
+router.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
 
 router.get('/messages', messagesController.getAll);
 router.post('/messages', messagesMiddleware.validateBody, messagesController.createMessage);
